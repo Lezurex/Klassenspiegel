9b3bd35c1213cb915ec374ed561b02bdfc83f471
Lezurex <lenny.angst@easyid.ch>
2020-08-31 22:39:38 +0200

    Hotfix login validation

--- js/login.js
+++ js/login.js
@@ -1,59 +1,52 @@
-$().ready(function () {
-    /**
-     * Removes error message when changed
-     */
-    $("#login-email").keyup(function () {
-        $("#login-email").removeClass("is-invalid");
-        $("#login-error").html("");
-        submitChangerLogin()
-    });
-    $("#login-password").keyup(function () {
-        $("#login-password").removeClass("is-invalid");
-        $("#login-error").html("");
-        submitChangerLogin()
-    });
-
-    /**
-     * E-Mail validation
-     */
-    $("#login-email").keyup(function () {
-        let email = $("#login-email").val();
-        if (validateEmail(email)) {
-            $("#login-email").removeClass("is-invalid");
-            $("#login-email").addClass("is-valid");
-        } else {
-            $("#login-email").removeClass("is-valid");
-            $("#login-email").addClass("is-invalid");
-        }
-    });
-
-    $("#login-email").unbind("keyup").bind("keyup", function (event) {
-        if (event.code === 'Enter') {
-            login();
-        }
-    });
-    $("#login-password").unbind("keyup").bind("keyup", function (event) {
-        if (event.code === 'Enter') {
-            login();
-        }
-    });
-
-    /**
-     * Register link
-     */
-    $("#login-btn-register").click(function (e) {
-        e.preventDefault();
-        $("#modal-login").modal("hide");
-        $("#modal-register").modal("show");
-    });
-
-    /**
-     * Login button
-     */
-    $("#login-login-btn").on("click", function (e) {
-        e.preventDefault();
+/**
+ * Removes error message when changed
+ */
+$("#login-email").on("keyup", function (event) {
+    $("#login-email").removeClass("is-invalid");
+    $("#login-error").html("");
+    submitChangerLogin();
+    if (event.code === 'Enter') {
         login();
-    });
+    }
+});
+$("#login-password").on("keyup", function (event) {
+    $("#login-password").removeClass("is-invalid");
+    $("#login-error").html("");
+    submitChangerLogin();
+    if (event.code === 'Enter') {
+        login();
+    }
+});
+
+/**
+ * E-Mail validation
+ */
+$("#login-email").on("keyup", function () {
+    let email = $("#login-email").val();
+    if (validateEmail(email)) {
+        $("#login-email").removeClass("is-invalid");
+        $("#login-email").addClass("is-valid");
+    } else {
+        $("#login-email").removeClass("is-valid");
+        $("#login-email").addClass("is-invalid");
+    }
+});
+
+/**
+ * Register link
+ */
+$("#login-btn-register").click(function (e) {
+    e.preventDefault();
+    $("#modal-login").modal("hide");
+    $("#modal-register").modal("show");
+});
+
+/**
+ * Login button
+ */
+$("#login-login-btn").on("click", function (e) {
+    e.preventDefault();
+    login();
 });
 
 function submitChangerLogin() {
@@ -63,6 +56,8 @@ function submitChangerLogin() {
         if (validateEmail($("#login-email").val())) {
             if ($("#login-password").val().length > 7) {
                 $("#login-login-btn").removeAttr("disabled");
+            } else {
+                $("#login-login-btn").attr("disabled", "disabled");
             }
         }
     }
@@ -83,22 +78,22 @@ function login() {
     }
 
     let data = {
-        'email':$("#login-email").val(),
-        'password':$("#login-password").val()
+        'email': $("#login-email").val(),
+        'password': $("#login-password").val()
     };
     $.ajax({
-        method:"POST",
-        url:"https://" + window.location.hostname + "/php/login.php",
+        method: "POST",
+        url: "https://" + window.location.hostname + "/php/login.php",
         data: data,
-        timeout:5000,
+        timeout: 5000,
         success: function (data) {
-            if(data == "901") {
+            if (data == "901") {
                 $("#login-email").addClass("is-invalid");
                 $("#login-password").addClass("is-invalid");
                 $("#login-http-error").html("Falsche E-Mail oder falsches Passwort!");
-            } else if(data == "905") {
+            } else if (data == "905") {
                 $("#login-http-error").html("Dein Konto wurde noch nicht freigeschaltet. Wende dich bei einer längeren Wartezeit an den Websiteadministrator.");
-            } else if(data == "200") {
+            } else if (data == "200") {
                 window.location.href = "/dashboard";
             } else {
                 $("#login-http-error").html("Es ist ein Fehler aufgetreten. Versuche es später noch einmal.");
