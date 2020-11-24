2304d6903355bfeb8315f1e49e6dd899e08cc6a8
Lenny Angst <48829956+Lezurex@users.noreply.github.com>
2020-11-24 09:35:07 +0100

    Fix BMS task button dark mode inversion

--- dashboard/aufgaben/index.php
+++ dashboard/aufgaben/index.php
@@ -28,7 +28,7 @@ if ($tasks == null) {
 <td>{$task['subject']}</td>";
                     $date = date("j. n. Y", $task['date']) . ' um ' . date('G:i', $task['date']);
                     $tableContent .= "<td>$date</td>";
-                    $tableContent .= '<td><button type="button" class="btn btn-primary task-btn-open" value="' . $task['id'] . '">Details</button></td>';
+                    $tableContent .= '<td><button type="button" class="btn btn-primary task-btn-open inverted" value="' . $task['id'] . '">Details</button></td>';
                 }
             }
 
